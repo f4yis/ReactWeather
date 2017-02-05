6a428d1d9b911f701e293368240d5524c3e5bd57
Mohamed Fayis <Mohamed Fayis>
2017-02-05 21:07:45 +0530

    redirected

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 let app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-
+app.use((req, res, next) => {
+	if(req.headers['x-forwarded-proto'] === 'http'){
+		next();
+	}else{
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
 app.use(express.static('public'));
 
-app.listen(port,() => {
-  console.log("Running on ", port);
+app.listen(PORT ,() => {
+  console.log("Running on ", PORT);
 });
