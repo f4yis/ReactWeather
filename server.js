66ab38789964be5b5c4917bfb10f6dd363ae8447
Mohamed Fayis <Mohamed Fayis>
2017-02-05 20:58:16 +0530

    portchanged

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 let app = express();
+const port = process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
 
-app.listen(3000,() => {
-  console.log("Running");
+app.listen(port,() => {
+  console.log("Running on ", port);
 });
